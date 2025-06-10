be92af5ecd80363b88065a2d00b7af9f8ffdd09b
DELL <DELL@DESKTOP-623ERR5>
2025-06-10 09:49:19 +0530

    LiveProject

--- 7RMartSuperMarketProject/src/test/java/testscript/LoginTest.java
+++ 7RMartSuperMarketProject/src/test/java/testscript/LoginTest.java
@@ -10,7 +10,7 @@ import pages.LoginPage;
 import utilities.ExcelUtility;
 
 public class LoginTest extends Base {
-	@Test(description = "Verify wheather  user can login with valid credentials")
+	@Test(description = "Verify wheather  user can login with valid credentials",priority=1)
 	public void verify_UserLoginwithValidCredentials() throws IOException {
 		String username = ExcelUtility.getStringData(0, 0, "LoginPage");
 		String password = ExcelUtility.getStringData(0, 1, "LoginPage");
@@ -22,7 +22,7 @@ public class LoginTest extends Base {
 
 	}
 
-	@Test(description = "Verify wheather user can login with invalid username and valid password")
+	@Test(description = "Verify wheather user can login with invalid username and valid password",priority=2)
 	public void verifyUserLoginwith_InValidUsernameandvalidpassword() throws IOException
 
 	{
@@ -36,7 +36,7 @@ public class LoginTest extends Base {
 		loginPage.clickOnSignInButton();
 	}
 
-	@Test(description = "Verify wheather usercan login with valid username and invalid password")
+	@Test(description = "Verify wheather usercan login with valid username and invalid password",priority=3)
 	public void verifyUserLoginwith_ValidUsernameandInValidPassword() throws IOException {
 		String username = ExcelUtility.getStringData(2, 0, "LoginPage");
 		String password = ExcelUtility.getStringData(2, 1, "LoginPage");
@@ -47,7 +47,7 @@ public class LoginTest extends Base {
 		loginPage.clickOnSignInButton();
 	}
 
-	@Test(description = "Verify wheather user can login with invalid credentials")
+	@Test(description = "Verify wheather user can login with invalid credentials",priority=4)
 	public void verifyUserLoginwith_InValidCredentials() throws IOException {
 
 		String username = ExcelUtility.getStringData(3, 0, "LoginPage");
